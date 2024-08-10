3f9b1ac206d42861945f8a7812551ba27aa3c7f2
Helyousfi <visioncomputer200@gmail.com>
2024-08-10 14:31:36 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISPLAY_NAME = os.getenv('display_name')
+SENDER_EMAIL = os.getenv('sender_email')
+PASSWORD = os.getenv('password')
+
+try:
+    assert DISPLAY_NAME
+    assert SENDER_EMAIL
+    assert PASSWORD
+except AssertionError:
+    #print('Please set up credentials.')
+    pass
+else:
+    #print('Credentials loaded successfully')
+    pass
